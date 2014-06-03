13e1ac268f9cd690dc2ac279d3f1ea91adc6b937
Eric Bravo <eric.bravo.rod@gmail.com>
2014-06-03 08:39:48 -0500

    Ajustando configuración
    - Cambiando la estructora de directorios
    - Versionando archivo de configuración y conexión a base de datos

--- app/controllers/cms.php
+++ app/controllers/cms.php
@@ -1,13 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Cms extends CI_Controller {
+
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('cms_model', 'cms');
 	}
 
 	public function index() {
-
 		if ($this->session->userdata('session') !== TRUE) {
 			redirect("login");
 		} else {
