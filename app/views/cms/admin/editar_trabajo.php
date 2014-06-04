69165087d8ddc34f9cdfd25efccd132cff1af3a1
Eric Bravo <eric.bravo.rod@gmail.com>
2014-06-04 14:55:00 -0500

    Quitando debug
    - Quitando las 2 primeras lineas de variables en vardump

--- app/views/cms/admin/editar_trabajo.php
+++ app/views/cms/admin/editar_trabajo.php
@@ -1,7 +1,7 @@
 <?php /* 29/05 */ 
 
-var_dump($cron_date);
-var_dump($trabajo_editar); 
+//var_dump($cron_date);
+//var_dump($trabajo_editar); 
 
 $meses = array(1=>"Enero",2=>"Febrero",3=>"Marzo",4=>"Abril",5=>"Mayo",6=>"Junio",7=>"Julio",8=>"Agosto",9=>"Septiembre",10=>"Octubre",11=>"Noviembre",12=>"Diciembre");
 $dias = array(0=>"Domingo",1=>"Lunes",2=>"Martes",3=>'Mi&eacute;rcoles',4=>"Jueves",5=>"Viernes",6=>'S&aacute;bado');
