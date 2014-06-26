b029c2988591b982422a80f0d2495f398df54b03
Eric Bravo <eric.bravo.rod@gmail.com>
2014-06-26 18:54:27 -0500

    Quitando scripts y css no utilizados

--- app/views/cms/header.php
+++ app/views/cms/header.php
@@ -9,20 +9,13 @@
 	<?php echo link_tag('css/bootstrap.min.css'); ?>
 	<?php echo link_tag('css/jquery-ui-1.10.4.custom.css'); ?>
 	<?php echo link_tag('css/colorbox.css'); ?>
-	<?php echo link_tag('css/aciTree.css'); ?>
 	<?php echo link_tag('css/middleware.css'); ?>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 	<script src="<?php echo base_url(); ?>js/jquery-ui-1.10.4.custom.js"></script>
 	<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.form.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.aciPlugin.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.aciTree.dom.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.aciTree.core.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.aciTree.checkbox.js"></script>
-	<!--<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.validate.js"></script>-->
 	<script type="text/javascript" src="<?php echo base_url(); ?>js/spin.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>js/middleware.js"></script>
-	<!--<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.colorbox.js"></script>-->
 </head>
 <body>
 	<div id="foo"></div>
