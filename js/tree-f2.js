9c0dd280fb8c948596c7fb307c95f80212cb6468
Eric Bravo <eric.bravo.rod@gmail.com>
2014-10-03 15:19:01 -0500

    Actualizando librerías ExtJS de detección y paseo de Feeds

--- js/tree-f2.js
+++ js/tree-f2.js
@@ -3,12 +3,14 @@ Ext.application({
     appFolder   : "./js/tvsa/",
     launch: function() {
         var me = this;
+
         function getJsonOfStore(store){
             var datar = new Array();
             var jsonDataEncode = "";
             var records = store.getRange();
-            for ( var i = 0; i < records.length; i++ )
-                datar.push( records[i].data );
+
+            for (var i = 0; i < records.length; i++)
+                datar.push(records[i].data);
             
             return Ext.encode(datar);
         }
@@ -16,92 +18,117 @@ Ext.application({
         TVSA = {
             _view    : null,
             _inputHidden : null,
-            resetAll: function(){
+            resetAll     : function(){
                 var me = this;
                 me.resetTemplate();
                 me.resetFeed();
                 me.resetGrid();
             },
-            resetTemplate: function(){
-                var me          = this,
-                    tree        = Ext.getCmp('tvsatemplate'),
-                    store       = tree.store,
-                    childrens   = store.getRootNode().childNodes;
 
-                    if ( childrens.length > 0 )
+            resetTemplate : function(){
+                var me       = this,
+                    tree     = Ext.getCmp('tvsatemplate'),
+                    store    = tree.store,
+                    childrens = store.getRootNode().childNodes;
+
+                    if(childrens.length > 0)
                         store.getRootNode().collapse();
 
                     store.getRootNode().removeAll();
             },
-            resetFeed: function(){
-                var me          = this,
-                    tree        = Ext.getCmp('tvsafeed'),
-                    store       = tree.store,
-                    childrens   = store.getRootNode().childNodes;
-                    if ( childrens.length > 0 )
+
+            resetFeed    : function(){
+                var me       = this,
+                    tree     = Ext.getCmp('tvsafeed'),
+                    store    = tree.store,
+                    childrens = store.getRootNode().childNodes;
+
+                    if(childrens.length > 0)
                         store.getRootNode().collapse();
 
                     store.getRootNode().removeAll();
             },
-            resetGrid: function(){
+
+            resetGrid    : function(){
                 var me      = this,
                     grid    = Ext.getCmp('tvsagrid'),
                     store   = grid.store;
+
                     store.removeAll();
             },
-            save: function(){
-                var me          = this,
-                data        = [];
-                if ( me.save.arguments.length > 0 ){
-                    var data = me.save.arguments[0];
-                    me._inputHidden = data;
-                    Ext.get(data).dom.value = me.getData();
-                }
-            },
-            getData: function(){
-                var me      = this,
-                grid        = Ext.getCmp( 'tvsagrid' ),
-                store       = grid.store;
-                return base64_encode( getJsonOfStore( store ) );
-            },
-            create: function(){
-                var me          = this,
-                data        = [];
-                if ( me.create.arguments.length > 0 )
-                    var data = me.create.arguments[0];
 
-                me._view = Ext.create( 'TVSA.Viewport', Ext.applyIf( data, {
-                } ) );
+            save     : function(){
+                var me          = this,
+                    data        = [];
+
+                    if(me.save.arguments.length > 0)
+                    {
+                        var data = me.save.arguments[0];
+                        me._inputHidden = data;
+                        Ext.get(data).dom.value = me.getData();
+                    }
             },
-            show: function(){
+
+            getData  : function(){
+                var me      = this,
+                    grid    = Ext.getCmp('tvsagrid'),
+                    store   = grid.store;
+
+                    return base64_encode(getJsonOfStore(store));
+
+
+            },
+
+            create   : function(){
+                var me          = this,
+                    data        = [];
+
+                    if(me.create.arguments.length > 0)
+                        var data = me.create.arguments[0];
+
+                    me._view = Ext.create('TVSA.Viewport',Ext.applyIf(data,{
+                    }));
+            },
+
+            show    : function(){
                 var me   = this,
-                id       = Ext.getBody();
-                if ( me.show.arguments.length > 0 )
-                    var id = me.show.arguments[0];
-                if ( ! me._view )
-                    me.create({ renderTo:id });
+                    id   = Ext.getBody();
+
+                    if(me.show.arguments.length > 0)
+                        var id = me.show.arguments[0];
+
+                if(!me._view)
+                    me.create({renderTo:id});
                 else
                     me._view.show();
             },
+
             hide: function () {
                 var me   = this;
-                if ( me._view )
+
+                if(me._view)
                     me._view.hide();
             },
-            setFeed: function( url ){
+
+            setFeed : function(url){
                 var me       = this,
-                tvsafeed = Ext.getCmp('tvsafeed');
+                    tvsafeed = Ext.getCmp('tvsafeed');
+
                 me.resetFeed();
-                setTimeout( function(){
-                    tvsafeed.loadData( url );
+
+                setTimeout(function(){
+                    tvsafeed.loadData(url);
                 }, 500);
             },
-            setTemplate: function( url ){
+
+            setTemplate : function(url){
                 var me       = this,
-                tvsafeed = Ext.getCmp( 'tvsatemplate' );
+                    tvsafeed = Ext.getCmp('tvsatemplate');
+
                 me.resetTemplate();
-                setTimeout( function(){
-                    tvsafeed.loadData( url );
+
+                setTimeout(function(){
+                    tvsafeed.loadData(url);
                 }, 500);
             }
         };
