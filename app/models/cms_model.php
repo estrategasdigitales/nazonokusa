b14046b620a9d7d70104026f7b6296eb21396781
Miguel Angel Martinez Zamora <natacion@gmail.com>
2014-12-09 19:00:46 -0600

    output

--- app/models/cms_model.php
+++ app/models/cms_model.php
@@ -217,7 +217,7 @@ class Cms_model extends CI_Model {
             $this->db->join( $this->db->dbprefix('verticales'). ' AS c', 'a.uid_vertical = c.uid_vertical','INNER' );
             $this->db->join( $this->db->dbprefix('estructuras_salida'). ' AS d', 'a.plantilla = d.uid_estructura','LEFT' );
 
-            $this->db->order_by("id_trabajo", "desc");
+            $this->db->order_by("a.id_trabajo", "desc");
 
             if ( ! empty( $limit ) )
                 $this->db->limit( $limit, $start );
