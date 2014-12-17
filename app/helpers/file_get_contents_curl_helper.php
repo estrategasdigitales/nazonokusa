fe2bb93751a3e7a080dfbbedeb6729f194992f98
Eric Bravo <eric.bravo.rod@gmail.com>
2014-12-17 12:43:37 -0600

    Cambiando un parámetro en el curl para permitir https

--- app/helpers/file_get_contents_curl_helper.php
+++ app/helpers/file_get_contents_curl_helper.php
@@ -9,7 +9,7 @@ if ( ! function_exists('file_get_contents_curl') ){
 		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 
 		$data = curl_exec($ch);
